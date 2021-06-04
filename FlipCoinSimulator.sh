4d8c42424609e9d6a2e9862cb5ff6f796507f362
Chandan Tiwary <ctiwary36@gmail.com>
2021-06-04 23:27:54 +0530

    Added:UC3_To_Doublet_Flip_Coin_simulator

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,28 +1,40 @@
 echo "FLIP_COIN_SIMULATION"
-HEAD=0
+isHEAD=0
+NUMBER_OF_COIN=2
 
-headCount=0
-tailCount=0
 
-declare -A singletFlip
+declare -A doubletFlip
 
 
 read -p "Enter the Number of Coin Flip : " numberOfCoinFlip
+function doublet()
+{
+   for(( count=0; count<$numberOfCoinFlip; count++ ))
+   do
+      for(( countCoin=0; countCoin<$NUMBER_OF_COIN; countCoin++ ))
+      do
+         flipCoin=$(( RANDOM%2 ))
 
-for(( count=0; count<$numberOfCoinFlip; count++ ))
-do
-   FlipCoin=$(( RANDOM%2 ))
+         if [ $FlipCoin -eq $isHEAD ]
+         then
+            coinSide+=H
+         else
+            coinSide+=T
+         fi
+		done
+		((doubletFlip[$coinSide]++))
+		coinSide=""
+	done
 
-   if [ $FlipCoin -eq $HEAD ]
-   then
-      singletFlip[HEAD]=$((++headCount))
-   else
-      singletFlip[TAIL]=$((++tailCount))
-   fi
-done
+function totalDoubletPercentage()
+{
+   for index in ${!doubletFlip[@]}
+   do
+      doubletFlip[$index]=$(( ({doubletFlip[$index]}*100)/numberOfCoinFlip ))
+   done
 
-singletHeadPercentage=$(( (headCount*100)/numberOfCoinFlip ))
-singletTailPercentage=$(( tailCount*100)/numberOfCoinFlip ))
+}
 
-echo "To single head percentage and head count:$headCount : " $singletHeadPercentage
-echo "To single tail percentage and tail count:$tailCount : " $singletTailPercentage
+
+doublet
+totalDoubletPercentage
